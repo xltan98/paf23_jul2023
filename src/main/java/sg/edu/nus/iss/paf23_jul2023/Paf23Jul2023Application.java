5299423df55d8fba4057066a128211099f880bce
xltan98 <xinnli98@gmail.com>
2023-06-22 14:05:37 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/paf23_jul2023/Paf23Jul2023Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf23_jul2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paf23Jul2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Paf23Jul2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/paf23_jul2023/Paf23Jul2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf23_jul2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Paf23Jul2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
